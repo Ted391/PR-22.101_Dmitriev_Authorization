7ea5cd2e9259d8ff51f966f73764f01d5e656859
teddy <anatoliy.dmitriev06@mail.ru>
2025-01-23 17:09:29 +0700

    Добавьте файлы проекта.

---
+++ PR-22.101_Dmitriev_Authorization/Helper.cs
@@ -0,0 +1,23 @@
+﻿using PR_22._101_Dmitriev_Authorization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_22._101_Dmitriev_Authorization
+{
+    internal class Helper
+    {
+        private static DmitrievKR_ProgModulesEntities _context;
+
+        public static DmitrievKR_ProgModulesEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new DmitrievKR_ProgModulesEntities();
+            }
+            return _context;
+        }
+    }
+}
---
+++ PR-22.101_Dmitriev_Authorization/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PR_22._101_Dmitriev_Authorization.Models
+
+namespace PR_22._101_Dmitriev_Authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = tb_username.Text;
+            string password = tb_password.Text;
+
+            User user = 
+        }
+    }
+}
